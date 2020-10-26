22d0e8a55092a73de60fa29119897c9731befaf8
DigitalDW <loris.rimaz@gmail.com>
2020-10-26 11:29:15 +0100

    Basic timer, advantages: can be saved in a file

--- scripts/main.js
+++ scripts/main.js
@@ -7,11 +7,27 @@ class Main extends Phaser.Scene {
       hunger: 0,
     };
     this.mental = 0;
+    this.test;
+    this.seconds = 0;
   }
 
   preload() {}
 
-  create() {}
+  create() {
+    const timerEventConfig = {
+      delay: 1000,
+      repeat: 0,
+      loop: true,
+      callback: this.displayTime,
+      callbackScope: this,
+    };
+    this.test = this.time.addEvent(timerEventConfig);
+  }
+
+  displayTime() {
+    this.seconds++;
+    console.log(this.seconds);
+  }
 
   update() {}
 
