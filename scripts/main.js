5934dbbd0dff5b02e15f11170cce55471de1e7cc
ratdo <55990722+ratdo@users.noreply.github.com>
2020-11-10 16:52:44 +0100

    Player sprites, basic movements and borders

--- scripts/main.js
+++ scripts/main.js
@@ -9,9 +9,14 @@ class Main extends Phaser.Scene {
     this.mental = 0;
     this.test;
     this.seconds = 0;
+    this.playerSpeed = 3;
+    this.cursors;
   }
 
-  preload() {}
+  preload() {
+    // preload l'image du joueur
+    this.load.image('player', '../assets/redb.png');
+  };
 
   create() {
     const timerEventConfig = {
@@ -22,6 +27,13 @@ class Main extends Phaser.Scene {
       callbackScope: this,
     };
     this.test = this.time.addEvent(timerEventConfig);
+
+  
+    // créer le sprite du joueur
+    this.player = this.physics.add.image(100, 100, 'player');
+    this.player.setCollideWorldBounds(true);
+    // créer les variables d'input flèches du clavier
+    this.cursors = this.input.keyboard.createCursorKeys();
   }
 
   displayTime() {
@@ -33,6 +45,17 @@ class Main extends Phaser.Scene {
     if (this.seconds >= 10 && this.test.paused == false) {
       console.log('stop');
       this.test.paused = true;
+    }
+       //regarder si input actif j'ai fait avec if et else mais un switch pourrait être mieux
+    if(this.cursors.right.isDown){
+      //faire avancer le personnage 
+      this.player.x += this.playerSpeed;
+    } else if(this.cursors.left.isDown){
+      this.player.x += -this.playerSpeed;
+    } else if(this.cursors.down.isDown){
+      this.player.y += this.playerSpeed;
+    } else if(this.cursors.up.isDown){
+      this.player.y += -this.playerSpeed;
     }
   }
 
